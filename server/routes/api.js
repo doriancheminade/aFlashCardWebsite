dee23adc4df6f099d91e02a12eb55b739454ce00
doriancheminade <cheminadedorian@gmail.com>
2017-05-29 15:49:44 +0200

    set up node server, and mongodb for the api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-const axios = require('axios');
-const API = 'https://jsonplaceholder.typicode.com';
+var DB = require('../../db')
+var db = DB.getDB()
+var flashcards = "flashcardDB"
+var FLASHCARD_URI = 'mongodb://localhost:27017/'+flashcards
+
+
+DB.connect(FLASHCARD_URI, function(err){
+    if(err){return console.dir(err);}
+    DB.drop(function(done){
+        if(err){return console.dir(err);}
+        DB.fixtures(flashcards, function(done){
+
+        })
+    })
+})
+router.get('/getAllFlashCards', (req, res) => {
+    DB.getDB().collection(flashcards).find().toArray(function (err, result) {
+        if (err) {
+            console.error('Find failed', err);
+        } else {
+            console.log('Find successful', result);
+            res.send({ 'cards': result});
+        }
+    })
+});
 
 router.get('/', (req, res) => {
   res.send('api works');
-});
-
-router.get('/posts', (req, res) => {
-  
-  
-  axios.get(`${API}/posts`)
-    .then(posts => {
-      res.status(200).json(posts.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
 });
 
 module.exports = router;
